3a27217f49d9562f7326376a5bc9c144a165bcda
Demo User <debian@beaglebone.localdomain>
2020-12-14 06:49:36 +0000

    setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install cmake
+
+
+cd ../ && git clone https://github.com/osresearch/LEDscape.git && cd LEDscape && make
+
+cd ../ && git clone https://github.com/warmcat/libwebsockets.git && cd libwebsockets && mkdir build && cmake ../ && make && sudo make install
+
+
+echo 'options uio_pruss extram_pool_sz=0x800000' | sudo tee -a /etc/modprobe.d/ledscape.conf
+
