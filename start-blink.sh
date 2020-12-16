ea61b62d429752de088320e15c7442316ec21bf8
Demo User <debian@beaglebone.localdomain>
2020-12-16 06:14:45 +0000

    add startup script

--- start-blink.sh
+++ start-blink.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 
-echo 'CAPE-BONE-OCTO' | sudo tee -a /sys/devices/bone_capemgr.*/slots
+#echo 'CAPE-BONE-OCTO' | sudo tee -a /sys/devices/bone_capemgr.*/slots
+
+#echo 'stop' | sudo tee /sys/class/remoteproc/remoteproc1/state
+echo 'ledpru-fw' | sudo tee /sys/class/remoteproc/remoteproc1/firmware
+echo 'start' | sudo tee /sys/class/remoteproc/remoteproc1/state
+
+config-pin p9_31 pruout
 
 cd /home/debian/ublinken
-LD_LIBRARY_PATH=/usr/local/lib ./ublink-ws
+#LD_LIBRARY_PATH=/usr/local/lib ./ublink-ws
+sudo ./ublink-ws
