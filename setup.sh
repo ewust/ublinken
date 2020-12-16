687f39dd54f9c0495d0b62b56ef7dbf6ddb47ca8
Demo User <debian@beaglebone.localdomain>
2020-12-16 07:00:18 +0000

    something like that

--- setup.sh
+++ setup.sh
@@ -4,10 +4,13 @@ sudo apt update
 sudo apt install cmake
 
 
-cd ../ && git clone https://github.com/osresearch/LEDscape.git && cd LEDscape && make
+#cd ../ && git clone https://github.com/osresearch/LEDscape.git && cd LEDscape && make
+cd ../ && git clone https://github.com/ewust/ledpru && cd ledpru/pru && make && sudo make install && sudo make config && sudo make run && cd .. && make
 
 cd ../ && git clone https://github.com/warmcat/libwebsockets.git && cd libwebsockets && mkdir build && cmake ../ && make && sudo make install
 
+#echo 'options uio_pruss extram_pool_sz=0x800000' | sudo tee -a /etc/modprobe.d/ledscape.conf
 
-echo 'options uio_pruss extram_pool_sz=0x800000' | sudo tee -a /etc/modprobe.d/ledscape.conf
+make
+sudo make install
 
